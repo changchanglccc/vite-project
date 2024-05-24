4f8f84c8d68491fd6901bbf8cd5c471ed99d484c
changchanglccc <jennifer06091992@gmail.com>
2024-05-23 21:02:05 -0400

    test: add e2e tests for home page left navigator section

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,28 @@
 describe('MaintainX QA-Take-Home Project home page', () => {
-    it('should be visitable', () => {
+    beforeEach(() => {
         // since we already set the baseUrl in cypress.config.ts file
         cy.visit('/');
     });
+
+    describe('left navigator section', () => {
+        it('should be visible', () => {
+            cy.get('nav').should('be.visible');
+        });
+
+        it('should include two list items', () => {
+            cy.get('nav ul li').should('have.length', 2);
+        });
+
+        it('should have the first anchor element named "Create Work Order“', () => {
+            cy.get('nav ul li a').eq(0).should('contain', 'Create Work Order');
+        });
+
+        it('should have the second anchor element named "View Work-Orders“', () => {
+            cy.get('nav ul li a').eq(1).should('contain', 'View Work-Orders');
+        });
+    });
+
+    describe('right articles section', () => {});
+
+    describe('home page screenshot', () => {});
 });
