accb77c0ab3519b31827a5b50bb8a3ba53f5f06f
changchanglccc <jennifer06091992@gmail.com>
2024-05-24 00:53:22 -0400

    feat: add cypress-allure-plugin

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "cypress";
+import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+
+      return config;
     }
   },
 });
