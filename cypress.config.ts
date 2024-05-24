19c4df42e3633c6a4a9d26fae36e24a97183f37d
changchanglccc <jennifer06091992@gmail.com>
2024-05-24 01:52:38 -0400

    feat: run allure locally
    - `allure serve allure-result`
    - dont want to put .mp4 videos to github
    - serve at "http://127.0.0.1:xxxxx", port could change, depends
    - add cypress/fixtures/allure-reporting-example.png manually screenshot

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,11 +4,17 @@ import allureWriter from "@shelex/cypress-allure-plugin/writer";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       allureWriter(on, config);
 
       return config;
+    },
+    env: {
+      allure: true,
+      allureAttachRequests: true,
+      allureAddVideoOnPass: true
     }
-  },
+  }
 });
