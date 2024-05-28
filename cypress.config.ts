1463dd348938e578c8674358fd14ccc3ef1f0f93
changchanglccc <jennifer06091992@gmail.com>
2024-05-28 16:14:26 -0400

    chore: remove unused code

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ import { allureCypress } from "allure-cypress/reporter";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on) {
       // implement node event listeners here
       allureCypress(on, {
         resultsDir: './allure-results'
